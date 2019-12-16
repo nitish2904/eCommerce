6a638b5c26de00d3208e95b74769f3371abbd6de
nitish2904 <34388026+nitish2904@users.noreply.github.com>
2019-12-16 21:18:40 +0530

    login intent done

---
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button joinNowButton,LoginButtton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        joinNowButton = (Button)findViewById(R.id.main_join_now_btn);
+        LoginButtton = (Button)findViewById(R.id.main_login_btn);
+
+        LoginButtton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
