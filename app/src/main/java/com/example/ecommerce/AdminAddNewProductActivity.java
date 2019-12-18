c791822ff6297a4a801ab16fbf28a1efa8bbe304
nitish2904 <34388026+nitish2904@users.noreply.github.com>
2019-12-19 01:47:33 +0530

    ui added for add new product and some backend

--- app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
@@ -3,15 +3,26 @@ package com.example.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
     private String categoryName;
+    private Button AddNewProductButton;
+    private ImageView InputProductImage;
+    private EditText InputProductName,InputProductPrice,InputProductDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
         categoryName = getIntent().getExtras().get("category").toString();
-        Toast.makeText(this, categoryName, Toast.LENGTH_SHORT).show();
+        AddNewProductButton = (Button)findViewById(R.id.add_new_product);
+        InputProductDescription = (EditText)findViewById(R.id.product_description);
+        InputProductImage = (ImageView)findViewById(R.id.select_product_image);
+        InputProductPrice = (EditText)findViewById(R.id.product_price);
+        InputProductName = (EditText)findViewById(R.id.product_name);
+
     }
 }
